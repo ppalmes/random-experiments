af12f9efcb576d96d57c649e039f69f60291ca50
Paulito Palmes, PhD <ppalmes@gmail.com>
2022-02-18 18:45:39 +0000

    add sample file

---
+++ rllib.py
@@ -0,0 +1,15 @@
+import ray
+from ray import tune
+
+ray.init(address="example-ray-head")
+tune.run(
+    "PPO",
+    stop={"episode_reward_mean": 200},
+    config={
+        "env": "CartPole-v0",
+        "num_gpus": 1,
+        "num_workers": 10,
+        "lr": tune.grid_search([0.01, 0.001, 0.0001]),
+    },
+)
+
